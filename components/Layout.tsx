87f0936ecd461a666de4585bfe700cf866413dc4
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-12-27 21:41:53 +0900

    created Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import { FC, ReactNode } from 'react'
+
+type Props = {
+  title: string
+  children: ReactNode
+}
+
+export const Layout: FC<Props> = ({ children, title = 'Mantaine' }) => {
+  return (
+    <div className="flex min-h-screen">
+      <Head>
+        <title>{title}</title>
+        <header></header>
+        <main className="flex flex-1 flex-col justify-center p-4">
+          {children}
+        </main>
+        <footer></footer>
+      </Head>
+    </div>
+  )
+}
