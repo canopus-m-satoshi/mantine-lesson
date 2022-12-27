f450ca5209df8ec52771d08e3dcea2dfe6d43762
Satoshi Matsushima <work.satoshi1993@gmail.com>
2022-12-27 22:35:23 +0900

    learning button component

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,6 +1,28 @@
+import { Button, Group } from '@mantine/core'
+import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
+
 import { Layout } from '../components/Layout'
 
 const ButtonDemo = () => {
-  return <Layout title="Button">button</Layout>
+  return (
+    <Layout title="Button">
+      <Group direction="column" position="center">
+        <Button
+          classNames={{
+            leftIcon: 'text-pink-500 h-6 w-6',
+            rightIcon: 'text-orange-500 h-6 w-6',
+          }}
+          color="cyan"
+          radius="lg"
+          uppercase
+          leftIcon={<BrandGithub />}
+          rightIcon={<ThreeDCubeSphere />}
+        >
+          Press
+        </Button>
+        <Button mt="md">Click</Button>
+      </Group>
+    </Layout>
+  )
 }
 export default ButtonDemo
